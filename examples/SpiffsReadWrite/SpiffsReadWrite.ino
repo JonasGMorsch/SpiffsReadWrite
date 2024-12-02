ef57b1c218f58e48b1a21bb081b5685f9d7b702b
Jonas G. Morsch <jonasgmorsch@gmail.com>
2024-12-02 13:46:33 -0300

    Update SpiffsReadWrite.ino

--- examples/SpiffsReadWrite/SpiffsReadWrite.ino
+++ examples/SpiffsReadWrite/SpiffsReadWrite.ino
@@ -8,7 +8,7 @@ void setup()
   Serial.println();
 
   Serial.printf("SPIFFS partition start address: %u \n", Spiffs.partition_address);
-  Serial.printf("SPIFFS partition size: %ukB or %u words \n", Spiffs.partition_size/1024, Spiffs.partition_size / 4U);
+  Serial.printf("SPIFFS partition size: %ukB or %u slots \n", Spiffs.partition_size/1024, Spiffs.partition_size / 4U);
 
   //Spiffs.format(); // Uncomment to format the entire SPIFFS partition
 
@@ -19,7 +19,7 @@ void setup()
   Spiffs.write(address_to_rw, write_data, sizeof(write_data));
 
   //   Read to flash
-  Serial.printf("From address %u, read 10 itens of SPIFFS partition:\n", address_to_rw);
+  Serial.printf("From address %u, read next 10 slots:\n", address_to_rw);
   uint32_t read_data[10];
   Spiffs.read(address_to_rw, read_data, sizeof(read_data)); // Read the first 10 items starting from address_to_rw
   for (uint32_t i = 0; i < sizeof(read_data) / 4U; i++)
